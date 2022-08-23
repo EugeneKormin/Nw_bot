96f093a75c6198ef5900f34dc7cebc04d888b2a3
resol <Lerozo>
2022-08-23 17:05:39 +1000

    attempt to analyse area of interest#2

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ def main():
         screen: Screen = Screen()
         img: np.array = screen.grab(x=x, y=y, w=w, h=h)
 
-        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
         """
         circles = cv.HoughCircles(img_gray, cv.HOUGH_GRADIENT, 1, img_gray.shape[0] / 8,
                                    param1=100, param2=60,
@@ -49,7 +47,7 @@ def main():
             keyboard.press_and_release("space")
             print('clicked')
 
-        max_val, max_loc = opencv.detect_pull(canvas=img, THRESHOLD=.975, PATH=r'images/circle_samples/circle.png')
+        max_val, max_loc = opencv.detect_pull(canvas=img, THRESHOLD=.7, PATH=r'images/circle_samples/circle.png')
         '''
         sleep(1)
         if not keyboard.is_pressed('space'):
@@ -65,24 +63,32 @@ def main():
             keyboard.press_and_release('space')
 
         x = max_loc[0]+5
-        y = max_loc[1]+10
+        y = max_loc[1]+15
 
         img_sample = cv.imread(r'images/circle_samples/circle.png')
         w = img_sample.shape[0]//2
-        h = img_sample.shape[1]//2-4
+        h = img_sample.shape[1]//2-2
 
         start = (x, y)
         end = (x+w, y+h)
 
-        blurred_image = cv.GaussianBlur(img, (5, 5), 0)
+        blurred_image = cv.GaussianBlur(img, (13, 13), 0)
 
         image = cv.rectangle(blurred_image, start, end, (255, 0, 0), 2)
 
-        color_ = blurred_image[x+(w//2), y+(h//2)]
+        cv.circle(image, (x+w//2, y+h//2), 1, (0, 255, 0), 3)
 
-        area_of_interest = blurred_image[x:x+w//2, y:y+h//2]
+        hsv_canvas = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        lower = np.array([76, 127, 20])
+        upper = np.array([179, 255, 255])
+        mask = cv.inRange(hsv_canvas, lower, upper)
 
-        cv.imshow("test", image)
+        res = cv.bitwise_not(img, mask)
+
+        color = blurred_image[x+w//2, y+h//2]
+        print(color)
+
+        cv.imshow("test", res)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
